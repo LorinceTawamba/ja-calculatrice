37e5805183a555900f10011c8c893432f0371e3e
Lorince TAWAMBA <lorince.tawamba@gmail.com>
2022-11-05 14:34:35 +0100

    Création des l'interface des traitement

--- src/traitement/InterCalcul.java
+++ src/traitement/InterCalcul.java
@@ -1,4 +1,8 @@
 package traitement;
 
 public interface InterCalcul {
+    public double add(double op1, double op2);
+    public double sous(double op1, double op2);
+    public double multi(double op1, double op2);
+    public double div(double op1, double op2);
 }
